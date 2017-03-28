a092e7c8bb8c6b30322c27e1590e0e89df6619a4
yashjakhotiya <mailsforyashj@gmail.com>
2017-03-28 16:59:00 +0530

    Added Normalization and few comments

--- accgyromag.c
+++ accgyromag.c
@@ -6,8 +6,7 @@
 #define MaxValue 450//(Suppose)The voltage corresponding to 1g
 #define MinValue 250//(Suppose)The voltage corresponding to -1g
 
-#define pi 3.1415926
-#define time 0.01
+#define time 0.01//define waittime in secs
 
 //The calibrating function for voltage to acceleration conversion
 double calibrate(double value, double upperl, double lowerl, double callow, double multiplier)
@@ -44,32 +43,40 @@ double maginput()
 	}
 //Wait For Function
 //Takes number of seconds as input and makes program wait till then
-void delay(double dly)
+void waitforsecs(double waittime)
 	{
-		const clock_t start = clock();
+		const clock_t start = clock();//clock at start
 		clock_t current;
 		do
 			{
-				current = clock();
+				current = clock();//clock right now
 			}
-		while((double)(current-start)/CLOCKS_PER_SEC < dly);
+		while((double)(current-start)/CLOCKS_PER_SEC < waittime);//check if time elapsed has reached waittime
 	}
 int main()
 	{	
 		for(;;)
 			{
 				//Initializations
-				double xacc,yacc,zacc;
-				double pitch,roll,yaw,accpitch,accroll,gyropitch=0,gyroroll=0,gyroyaw=0,magx,magy,magz,magyaw;
+				double pitch,roll,yaw;
+				double xacc,yacc,zacc,normacc,accpitch,accroll;
+				double gyropitch=0,gyroroll=0,gyroyaw=0;
+				double magx,magy,magz,normmag,magyaw;
 		
 				//Finding out actual accelerations from voltages
 				xacc=calibrate(accinput(),MaxValue,MinValue,-1,2);
 				yacc=calibrate(accinput(),MaxValue,MinValue,-1,2);
 				zacc=calibrate(accinput(),MaxValue,MinValue,-1,2);
 				
+				//Normalization
+				normacc=sqrt(xacc*xacc+yacc*yacc+zacc*zacc);
+				xacc*=normacc;
+				yacc*=normacc;
+				zacc*=normacc;
+				
 				//Finding roll and pitch with trigonometric functions
-				accroll= (atan2(yacc,zacc))*180/pi;
-				accpitch= (atan2(-xacc,sqrt(yacc*yacc+zacc*zacc)))*180/pi;
+				accroll= (atan2(yacc,zacc))*180/M_PI;
+				accpitch= (atan2(-xacc,sqrt(yacc*yacc+zacc*zacc)))*180/M_PI;
 				
 				//Finding roll, pitch and yaw from Gyro
 				gyroroll= gyroroll + gyroinput()*time;
@@ -83,6 +90,9 @@ int main()
 				//Integrating magnetometer
 				magx=maginput();
 				magy=maginput();
+				normmag=sqrt(magx*magx+magy*magy);
+				magx*=normmag;
+				magy*=normmag;
 				magyaw=atan2(magy*cos(roll)-magz*sin(roll), magx*cos(pitch)+magy*sin(roll)*sin(pitch)+magz*cos(roll)*sin(pitch));
 				yaw=0.98*gyroyaw + 0.02*magyaw;
 				
@@ -90,6 +100,7 @@ int main()
 				printf("Acceleration along axes is-\n");
 				printf("x axis: %1.3lfg, y axis: %1.3lfg, z axis: %1.3lfg\n",xacc,yacc,zacc);
 				printf("Pitch is %3.3lf degrees, Roll is %3.3lf and Yaw is %3.3lf  degrees\n",pitch,roll,yaw);
-				delay(time);
+				waitforsecs(time);
 			}
+		return 0;
 	}
